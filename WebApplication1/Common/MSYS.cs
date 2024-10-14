fc57b3d278ea7a1f42a0244d874b937a31bc615f
RuCiCa <110306076@nccu.edu.tw>
2024-10-14 14:20:20 +0800

    feat：新增MSYS跟TCSIO

---
+++ WebApplication1/Common/MSYS.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Common
+{
+    public class MSYS
+    {
+        public string VARNAME { get; set; }
+        public decimal NUMBER { get; set; }
+        public string VALUE { get; set; }
+        public string VARDESC { get; set; }
+        public string MODDATE { get; set; }
+        public string MODTIME { get; set; }
+        public string MODUSER { get; set; }
+    }
+}
---
+++ WebApplication1/Common/TCSIO.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Common
+{
+    public class TCSIO
+    {
+        public string TDATE { get; set; }
+        public string BHNO { get; set; }
+        public string DSEQ { get; set; }
+        public string DNO { get; set; }
+        public string CSEQ { get; set; }
+        public string STOCK { get; set; }
+        public string BSTYPE { get; set; }
+        public decimal? QTY { get; set; }
+        public string IOFLAG { get; set; }
+        public string? REMARK { get; set; }
+        public string? JRNUM { get; set; }
+        public string? TRDATE { get; set; }
+        public string? TRTIME { get; set; }
+        public string? MODDATE { get; set; }
+        public string? MODTIME { get; set; }
+        public string? MODUSER { get; set; }
+    }
+}
